83cf142f02fd0f6985bf23ab20818cab275fe84c
Prathmesh Kaygude <141936681+Prathm7875@users.noreply.github.com>
2024-05-01 23:19:10 +0530

    Create ChatBot.py

---
+++ ChatBot.py
@@ -0,0 +1,53 @@
+def chat_bot_system():
+    print("Enter Your Name : ", end="")
+    name = input()
+    print("Hello", name, "Welcome to AASWAD-Restaurent\n")
+    print("What would you like to order", name, "\n")
+
+    menu_options = ["Rice-Plate", "Samosa", "Vada-Pav", "Chole-Bhature", "Pohe"]
+    q_count = [0] * len(menu_options)
+    
+    while True:
+        for i, option in enumerate(menu_options):
+            print("Option", i + 1, ":", option)
+
+        print("\nI would like to have option : ", end="")
+        opt = int(input()) - 1
+        if opt >= len(menu_options):
+            print("Display relevant query")
+            continue
+        
+        print("\nYou Confirm order :", menu_options[opt])
+        q_count[opt] += 1
+        if q_count[opt] >= 5:
+            break
+
+        order = input("Do you want anything else (yes/no): ").strip().upper()
+        print()
+        if order == "YES":
+            continue
+        else:
+            break
+
+    your_order(menu_options, q_count)
+    print("\nYour total bill is", total_bill(q_count))
+    print("\nThanks for your order!")
+
+def total_bill(q_count):
+    ans = 0
+    prize = [50, 25, 25, 55, 25]
+    for i in range(len(q_count)):
+        ans += q_count[i] * prize[i]
+    return ans
+
+def your_order(menu_options, q_count):
+    print("Your Order is : ")
+    for i in range(len(q_count)):
+        if q_count[i] > 0:
+            print(menu_options[i], q_count[i])
+
+def main():
+    chat_bot_system()
+
+if __name__ == "__main__":
+    main()
